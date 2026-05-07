55df5ae2a11c30022960e838bffce6b8c1fcd207
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2026-05-07 20:34:22 +0200

    Klar med klassen Book

---
+++ src/main/java/jk/Book.java
@@ -0,0 +1,41 @@
+package jk;
+
+/**
+ * Författare: Johnny Battah
+ * Klassen Book representerar en bok i bibliotekssystemet
+ */
+
+public class Book extends LibraryItem {
+    private String author;
+    private String genre;
+    private int pages;
+
+    public Book(String id, String title, boolean isAvailable, String author, String genre, int pages) {
+        super(id, title, isAvailable);
+        this.author = author;
+        this.genre = genre;
+        this.pages = pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre(){
+        return genre;
+    }
+
+    public int getPages(){
+        return pages;
+    }
+
+    @Override
+    public String getInfo(){
+        return "ID: " + id + ", Titel: " + title + ", Författare: " + author + ", Genre: " + genre + ", Sidor: " + pages + ", Tillgänglig: " + isAvailable;
+    }
+
+
+
+
+
+}
