88775b938642025338883571a4bbb6b4a795236f
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2026-05-07 20:23:15 +0200

    Klar med klassen LibraryItem (för E)

--- src/main/java/jk/LibraryItem.java
+++ src/main/java/jk/LibraryItem.java
@@ -1,33 +1,42 @@
 package jk;
 
+/**
+ * Författare Johnny Battah
+ * Klassen LibraryItem är basklassen för saker som finns i biblioteket
+ */
+
 public class LibraryItem {
     protected String id;
     protected String title;
     protected boolean isAvailable;
 
-    public LibraryItem(String id, String title, boolean isAvailable){
-        this.id=id;
-        this.title=title;
-        this.isAvailable=isAvailable;
-    }
-
-    public String getId(){
-        return id;
-    }
-
-    public String getTitle(){
-        return title;
-    }
-
-    public boolean getIsAvailable(){
-        return isAvailable;
-    }
-
-    public void setIsAvailable(boolean isAvailable){
+    public LibraryItem(String id, String title, boolean isAvailable) {
+        this.id = id;
+        this.title = title;
         this.isAvailable = isAvailable;
     }
 
-    public void setTitle(String title){
-        this.title=title;
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean getIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInfo(){
+        return "ID: " + id + "Titel: " + title + ", Tillgänglig: " + isAvailable;
     }
 }
