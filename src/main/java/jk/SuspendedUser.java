32ba6578e6645b34e69f8cacd7b8201e8e2cde43
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2026-05-16 20:33:15 +0200

    Nästan klar med SuspendedUser klassen

---
+++ src/main/java/jk/SuspendedUser.java
@@ -0,0 +1,39 @@
+package jk;
+
+/**
+ * Författare: Johnny Battah
+ * Klassen SuspendedUser representerar en avstängd användare i bibliotekssystemet.
+ * Den används för att avgöra om en kund får låna eller inte
+ */
+
+public class SuspendedUser {
+    private String id;
+    private String userId;
+    private String reason;
+
+    public SuspendedUser(String id, String userId, String reason) {
+        this.id = id;
+        this.userId = userId;
+        this.reason = reason;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getInfo() {
+        return "ID: " + id + ", UserID: " + userId + ", Orsak: " + reason;
+    }
+}
