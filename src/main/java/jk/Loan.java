7e2f87cc35146d6b320a5a51d0a1d5599b37ace6
Johnny Battah <johnny.battah@elev.ga.ntig.se>
2026-05-22 15:59:38 +0200

    Börjat A nivå, Gjorde klassen Loan (temporärlig)

---
+++ src/main/java/jk/Loan.java
@@ -0,0 +1,41 @@
+package jk;
+
+public class Loan {
+    private String userId;
+    private String itemId;
+    private String itemType;
+
+    public Loan(String userId, String itemId, String itemType) {
+        this.userId = userId;
+        this.itemId = itemId;
+        this.itemType = itemType;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public String getInfo() {
+        return "User ID: " + userId + ", Item ID: " + itemId + ", Item Type: " + itemType;
+    }
+}
